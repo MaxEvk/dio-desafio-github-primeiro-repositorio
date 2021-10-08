2bd6eb14fe0a765ec49220f73cd53acfe0c82b70
MaxEvk <maxevk@gmail.com>
2021-10-08 11:24:20 -0300

    Teste Function

---
+++ Introdução a criação de websites com HTML5 e CSS3/Desenvolvimento avançado com JavaScript ES6/index.js
@@ -0,0 +1,10 @@
+var obj = {
+    showContext: function showContext() {
+        setTimeout(() => {
+            this.log('after 1000ms');
+        }, 1000);
+    },
+    log: function log(value) {
+        console.log(value);
+    }
+};
